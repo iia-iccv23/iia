92c3ab44eea45a6be68c11651eabd4cdc7c8190a
iia-iccv23 <127226631+iia-iccv23@users.noreply.github.com>
2023-08-18 15:49:43 +0300

    Added torchgc library

---
+++ torchgc/pytorch_grad_cam/utils/__init__.py
@@ -0,0 +1,4 @@
+from torchgc.pytorch_grad_cam.utils.image import deprocess_image
+from torchgc.pytorch_grad_cam.utils.svd_on_activations import get_2d_projection
+from torchgc.pytorch_grad_cam.utils import model_targets
+from torchgc.pytorch_grad_cam.utils import reshape_transforms
